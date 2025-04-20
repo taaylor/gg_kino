55331c86137ed20aeddd13cfd8e1e599346f4bfb
Kirill67tyar <kirillbogomolov.ric@gmail.com>
2025-04-21 00:33:18 +0300

    Удаляю example_command

--- auth-service/src/commands/example_command.py
+++
@@ -1,7 +0,0 @@
-import typer
-
-app = typer.Typer()
-
-@app.command()
-def example(parametr: str = "Some-command"):
-    typer.echo(f"Command: {parametr}")
--- auth-service/src/manage.py
+++ auth-service/src/manage.py
@@ -1,11 +1,9 @@
 import typer
-from commands import example_command
 from commands.createsuperuser import createsuperuser
+
 
 app = typer.Typer()
 
-# Регистрируем подкоманды
-app.add_typer(example_command.app, name="example-command")
 app.command()(createsuperuser)
 
 if __name__ == "__main__":
