541cbd3bb047204dbd074c40c6b3ebe0f17c0136
Maksim <jw999coding@gmail.com>
2025-04-22 17:06:11 +0300

    Создаю таблицы в схеме profile

--- auth-service/src/models/models.py
+++ auth-service/src/models/models.py
@@ -1,7 +1,8 @@
 import uuid
 
 from db.postgres import Base
-from sqlalchemy import ForeignKey, String
+from models.models_types import GenderEnum
+from sqlalchemy import ForeignKey, String, UniqueConstraint
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -12,10 +13,18 @@ class User(Base):
 
     id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    first_name: Mapped[str] = mapped_column(String(50), nullable=True)
-    last_name: Mapped[str | None] = mapped_column(String(50), nullable=True)
-    gender: Mapped[str | None] = mapped_column(String(50), nullable=True)
+    first_name: Mapped[str | None] = mapped_column(String(50))
+    last_name: Mapped[str | None] = mapped_column(String(50))
+    gender: Mapped[GenderEnum]
+    role_code: Mapped[str] = mapped_column(ForeignKey("profile.dict_roles.role"))
 
+    # обратная orm связь с ролью (many-to-one)
+    role: Mapped["DictRoles"] = relationship(
+        "DictRoles",
+        back_populates="users",
+    )
+
+    # обратная orm связь с cred (one-to-one)
     user_cred: Mapped["UserCred"] = relationship(
         "UserCred",
         back_populates="user",
@@ -24,8 +33,8 @@ class User(Base):
         cascade="all, delete-orphan",
     )
 
-    def __str__(self):
-        return f"<User(id={self.id}, username={self.username})>"
+    def __repr__(self):
+        return f"<{self.__class__.__name__}(id={self.id}, username={self.username})>"
 
 
 class UserCred(Base):
@@ -35,7 +44,48 @@ class UserCred(Base):
     user_id: Mapped[UUID] = mapped_column(
         UUID(as_uuid=True), ForeignKey("profile.user.id"), primary_key=True
     )
-    email: Mapped[str | None] = mapped_column(String(255), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
 
+    # обратная orm связь с user (one-to-one)
     user: Mapped["User"] = relationship("User", back_populates="user_cred", uselist=False)
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}(user_id={self.user_id})>"
+
+
+class DictRoles(Base):
+    __tablename__ = "dict_roles"
+    __table_args__ = {"schema": "profile"}
+
+    role: Mapped[str] = mapped_column(String(50), primary_key=True)
+    descriptions: Mapped[str | None]
+
+    # обратная связь с user (one-to-many)
+    users: Mapped[list["User"]] = relationship("User", back_populates="role")
+
+    # обратная связь с permission (one-to-many)
+    permissions: Mapped[list["RolesPermissions"]] = relationship(
+        "RolesPermissions", back_populates="role", lazy="joined"
+    )
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}(role={self.role})>"
+
+
+class RolesPermissions(Base):
+    __tablename__ = "roles_permissions"
+    __table_args__ = (
+        UniqueConstraint("role_code", "permission", name="role_permission_idx"),
+        {"schema": "profile"},
+    )
+
+    role_code: Mapped[str] = mapped_column(ForeignKey("profile.dict_roles.role"), primary_key=True)
+    permission: Mapped[str] = mapped_column(String(50))
+    descriptions: Mapped[str | None]
+
+    # обратная связь с role
+    role: Mapped["DictRoles"] = relationship("DictRoles", back_populates="permission")
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}(permission={self.permission})>"
