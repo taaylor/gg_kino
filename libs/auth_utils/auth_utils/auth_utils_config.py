8b1945214979f272e8ff04e4d8750e4cfcb68de7
Dmitry <thesfinks.game@gmail.com>
2025-04-30 21:59:58 +0300

    Настраиваю конфигурацию библиотеки авторизации

---
+++ libs/auth_utils/auth_utils/auth_utils_config.py
@@ -0,0 +1,52 @@
+import logging
+import dotenv
+from pydantic import BaseModel
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+ENV_FILE = dotenv.find_dotenv()
+
+logger = logging.getLogger(__name__)
+
+
+class Redis(BaseModel):
+    host: str = "localhost"
+    port: int = 6379
+    user: str = "redis_user"
+    password: str = "Parol123"
+    db: int = 0
+
+
+class AuthUtilsConfig(BaseSettings):
+    algorithm: str = "RS256"
+    denylist_enabled: bool = True
+    authjwt_denylist_token_checks: set = {"access"}
+    public_key: str = """
+    -----BEGIN PUBLIC KEY-----
+    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArW7XpysaZje95xChyW8u
+    L8xGDbvUezcygQNcYep97eM9Vurhk+5BSgNF5sQfW/IeMCH2EtbQi+tVuyTYcelG
+    Gs2Flln/AHXdE6UqiS3AKvRxuvZTWetf80AGcyl7Sax2SY+j6sPSwy/q+SpaxYMf
+    QOx0buewylTZ7MUkkepsDf7ZbYZfzyUOUUzGilDWeKskKNt9ujCdZYNy6+37xWru
+    LsIdrTFC1/ggReddwEM4/VNvK5q+go+SCDfVgfQ8LbMiCgkKyZ3fgeP+KFsbCL/d
+    iaqd0feQZY8tFMEttcBzQaZUny2pjJ+cBNmkRJG54vD1wl3ujSIfimJ2gQTmCPvf
+    iwIDAQAB
+    -----END PUBLIC KEY-----
+    """
+
+    redis: Redis = Redis()
+
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILE,
+        case_sensitive=False,
+        env_nested_delimiter="_",
+        extra="ignore",
+    )
+
+
+def _get_config() -> AuthUtilsConfig:
+    app_config = AuthUtilsConfig()
+    logger.info(f"auth_utils_conf.initialized: {app_config.model_dump_json()}")
+    return app_config
+
+
+auth_utils_conf = _get_config()
--- libs/auth_utils/auth_utils/check_auth.py
+++ libs/auth_utils/auth_utils/check_auth.py
@@ -1,9 +1,24 @@
 import logging
+
 from async_fastapi_jwt_auth import AuthJWT
 from async_fastapi_jwt_auth.auth_jwt import AuthJWTBearer
 from pydantic import BaseModel
+from redis import Redis
+
+
+from .auth_utils_config import auth_utils_conf
+
 
 logger = logging.getLogger(__name__)
+
+
+redis_conn = Redis(
+    host=auth_utils_conf.redis.host,
+    port=auth_utils_conf.redis.port,
+    username=auth_utils_conf.redis.user,
+    password=auth_utils_conf.redis.password,
+    db=auth_utils_conf.redis.db,
+)
 
 
 class LibAuthJWT(AuthJWT):
@@ -14,34 +29,27 @@ class LibAuthJWT(AuthJWT):
 
 class LibAuthJWTBearer(AuthJWTBearer):
     """Bearer с изолированной конфигурацией"""
+
     pass
 
 
 class JWTSettings(BaseModel):
-    authjwt_algorithm: str = "RS256"
-    authjwt_public_key: str
-    authjwt_access_token_expires: int = 300
-    authjwt_refresh_token_expires: int = 1200
-
-
-def load_jwt_settings():
-    public_key = """
------BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArW7XpysaZje95xChyW8u
-L8xGDbvUezcygQNcYep97eM9Vurhk+5BSgNF5sQfW/IeMCH2EtbQi+tVuyTYcelG
-Gs2Flln/AHXdE6UqiS3AKvRxuvZTWetf80AGcyl7Sax2SY+j6sPSwy/q+SpaxYMf
-QOx0buewylTZ7MUkkepsDf7ZbYZfzyUOUUzGilDWeKskKNt9ujCdZYNy6+37xWru
-LsIdrTFC1/ggReddwEM4/VNvK5q+go+SCDfVgfQ8LbMiCgkKyZ3fgeP+KFsbCL/d
-iaqd0feQZY8tFMEttcBzQaZUny2pjJ+cBNmkRJG54vD1wl3ujSIfimJ2gQTmCPvf
-iwIDAQAB
------END PUBLIC KEY-----
-"""
-    return JWTSettings(authjwt_public_key=public_key)
+    authjwt_algorithm = auth_utils_conf.algorithm
+    authjwt_public_key = auth_utils_conf.public_key
+    authjwt_denylist_enabled = auth_utils_conf.denylist_enabled
+    authjwt_denylist_token_checks = auth_utils_conf.authjwt_denylist_token_checks
 
 
 @LibAuthJWT.load_config
 def get_config():
-    return load_jwt_settings()
+    return JWTSettings()
+
+
+@AuthJWT.token_in_denylist_loader
+async def check_if_token_in_denylist(decrypted_token) -> bool:
+    jti = decrypted_token["jti"]
+    entry = redis_conn.get(jti)
+    return entry and entry == "true"
 
 
 auth_dep = LibAuthJWTBearer()
