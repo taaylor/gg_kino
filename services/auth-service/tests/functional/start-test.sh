c97cec265bbe1b84ecbe52e3e73289d56eed2583
Kirill67tyar <kirillbogomolov.ric@gmail.com>
2025-04-30 17:13:05 +0300

    Вейтер для pstgresql, всё работает.

--- services/auth-service/tests/functional/start-test.sh
+++ services/auth-service/tests/functional/start-test.sh
@@ -2,11 +2,14 @@
 echo "Запуск waiters..."
 ls
 python3 tests/functional/utils/wait_for_redis.py
+python3 tests/functional/utils/wait_for_postgres.py
 python3 tests/functional/utils/wait_for_auth-api.py
 
 echo "Запуск тестов..."
 
-pytest functional/src -v -rP
+# pytest functional/src -v -rP
+pytest tests/functional/src -v -rP
 echo "Повторный запуск провалившихся тестов через 5 сек..."
 sleep 5
-pytest functional/src -v --last-failed -rP
+pytest tests/functional/src -v --last-failed -rP
+# pytest functional/src -v --last-failed -rP
