60e518d9012659766e7fb3b731aecdf7ab10093a
Maksim <jw999coding@gmail.com>
2025-05-01 18:23:27 +0300

    Вношу правки

--- services/auth-service/tests/functional/plugins/fixture_postgres.py
+++ services/auth-service/tests/functional/plugins/fixture_postgres.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 import pytest_asyncio
-from sqlalchemy import func, text
+from sqlalchemy import func
 from sqlalchemy.ext.asyncio import AsyncAttrs, AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
 from tests.functional.core.settings import test_conf
@@ -32,29 +32,21 @@ class Base(AsyncAttrs, DeclarativeBase):
 async def async_session_maker():
     engine = create_async_engine(test_conf.postgres.ASYNC_DATABASE_URL, echo=True, future=True)
     async_session_maker = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
-    # создание таблиц перед открытием соединения не требуется,
-    # так как они создаются с помощью миграций в API
 
     yield async_session_maker
-
-    # дропаем все таблицы после завершения всех тестов
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
 
     await engine.dispose()
 
 
 @pytest_asyncio.fixture(name="pg_session")
-def pg_session(async_session_maker):
-    async def inner():
-        async with async_session_maker() as session:
-            await session.begin()  # Открываем транзакцию
-            try:
-                yield session
-            finally:
-                await session.rollback()
-                # очищаем таблицы после транзакции
-                for table in reversed(Base.metadata.sorted_tables):
-                    await session.execute(text(f"TRUNCATE {table.name} CASCADE;"))
+async def pg_session(async_session_maker):
+    async with async_session_maker() as session:
+        await session.begin()  # Открываем транзакцию
 
-    return inner
+        yield session
+
+        # очищаем таблицы после транзакции
+
+        for table in reversed(Base.metadata.sorted_tables):
+            await session.execute(table.delete())
+        await session.commit()
--- services/auth-service/tests/functional/src/test_roles.py
+++ services/auth-service/tests/functional/src/test_roles.py
@@ -16,12 +16,13 @@ class TestRoles:
         [
             (
                 {
-                    "path_uuid": "some-role",
-                    "role": "some-role",
+                    "test": True,  # valid test
+                    "path_uuid": "ANONYMOUS",
+                    "role": "ANONYMOUS",
                     "descriptions": "описание",
                     "permissions": [
-                        {"permisson": PermissionEnum.FREE_FILMS.value, "descriptions": "описание"},
                         {"permisson": PermissionEnum.ASSIGN_ROLE.value, "descriptions": "описание"},
+                        {"permisson": PermissionEnum.FREE_FILMS.value, "descriptions": "описание"},
                     ],
                     "cached_data": True,
                 },
@@ -32,8 +33,9 @@ class TestRoles:
             ),
             (
                 {
+                    "test": False,  # invalid test
                     "path_uuid": "RRRR",
-                    "role": "some-role",
+                    "role": "ANONYMOUS",
                     "descriptions": "описание",
                     "permissions": [
                         {"permisson": PermissionEnum.FREE_FILMS.value, "descriptions": "описание"},
@@ -46,9 +48,10 @@ class TestRoles:
                 },
             ),
         ],
-        ids=["Test valid role", "Test unknow role"],
+        ids=["Test valid role", "Test invalid role"],
     )
     async def test_get_role_detail(
+        self,
         pg_session: AsyncSession,
         make_get_request,
         redis_test,
@@ -82,8 +85,16 @@ class TestRoles:
         uri = f"/roles/{query_data.get("path_uuid")}"
 
         body, status = await make_get_request(uri=uri)
-        cache_data = await redis_test(key=f"role:{role_detail.role}", cached_data=True)
+        cache_data = await redis_test(
+            key=f"role:{role_detail.role}", cached_data=query_data.get("cached_data")
+        )
 
-        assert body == role_detail.model_dump()
-        assert status == HTTPStatus.OK
-        assert cache_data == role_detail.model_dump()
+        assert status == expected_answer.get("status")
+        if not query_data.get("test"):
+            assert body == {"detail": "объект не найден"}
+            assert cache_data is None, expected_answer.get("err_msg_cache")
+            return
+        assert body == role_detail.model_dump(mode="json")
+        assert cache_data == role_detail.model_dump(mode="json"), expected_answer.get(
+            "err_msg_cache"
+        )
