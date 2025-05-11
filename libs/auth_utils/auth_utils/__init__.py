553b246ede7c8678a2170a08f2f615004d892816
Dmitry <thesfinks.game@gmail.com>
2025-05-12 00:03:40 +0300

    Поправил замечания по ревью. Добавил описание декораторов.

--- libs/auth_utils/auth_utils/__init__.py
+++ libs/auth_utils/auth_utils/__init__.py
@@ -2,12 +2,12 @@ from .auth_decorators import access_permissions_check
 from .auth_utils_config import Permissions
 from .check_auth import LibAuthJWT, LibAuthJWTBearer, auth_dep, get_config
 
-__all__ = [
+__all__ = (
     "LibAuthJWT",
     "LibAuthJWTBearer",
     "get_config",
     "auth_dep",
     "Permissions",
     "access_permissions_check",
-]
+)
 __version__ = "0.1.0"
--- libs/tracer_utils/tracer_utils/tracer_decorator.py
+++ libs/tracer_utils/tracer_utils/tracer_decorator.py
@@ -1,13 +1,29 @@
 from functools import wraps
-from typing import Callable
+from typing import Any, Callable, Coroutine
 
 from opentelemetry import context, trace
 
 
-def traced(name: str = None):
-    def decorator(func: Callable):
+def traced(name: str = None) -> Callable:
+    """
+    Декоратор для трассировки выполнения асинхронных функций с использованием OpenTelemetry.
+
+    Args:
+        name (str, optional): Имя для создаваемого спана.
+        Если не указано, используется имя декорируемой функции.
+
+    Returns:
+        Callable: Декорированная функция, обёрнутая в спан для трассировки.
+
+    Пример использования:
+        @traced("custom_span_name")
+        async def example_function():
+            pass
+    """
+
+    def decorator[**P, R](func: Callable) -> Callable[P, Coroutine[Any, Any, R]]:
         @wraps(func)
-        async def wrapper(*args, **kwargs):
+        async def wrapper(*args: P.args, **kwargs: P.kwargs) -> R | None:
             tracer = trace.get_tracer(__name__)
             current_context = context.get_current()
             with tracer.start_as_current_span(name or func.__name__, current_context):
