3d05989d8226c2fc01ca8db43ec648f3b936d855
Kirill67tyar <kirillbogomolov.ric@gmail.com>
2025-06-01 00:29:48 +0300

    ASD

--- services/metric-service/etl/config.py
+++ services/metric-service/etl/config.py
@@ -5,25 +5,30 @@ import dotenv
 # from pydantic import BaseModel
 from pydantic_settings import BaseSettings  # , SettingsConfigDict
 
-# import os
-
-
 ENV_FILE = dotenv.find_dotenv()
 
 logger = logging.getLogger(__name__)
 
 
-class Kafka(BaseSettings):
-    host: str = "localhost"
-    port: int = 9094
-    user: str = "postgres"
-    password: str = "postgres"
-    db: str = "pg_db"
+class KafkaConfig(BaseSettings):
+    host_0: str = "kafka-0"
+    host_1: str = "kafka-1"
+    host_2: str = "kafka-2"
+    port: int = 9092
+
+    @property
+    def bootstrap_servers(self):
+        return [
+            f"{self.host_0}:{self.port}",
+            f"{self.host_1}:{self.port}",
+            f"{self.host_2}:{self.port}",
+        ]
 
 
-class ClickHouse(BaseSettings):
+class ClickHouseConfig(BaseSettings):
     host: str = "localhost"
-    port: int = 5432
-    user: str = "postgres"
-    password: str = "postgres"
-    db: str = "pg_db"
+    port: int = 9000
+
+
+kafka_config = KafkaConfig()
+clickhouse_config = ClickHouseConfig()
