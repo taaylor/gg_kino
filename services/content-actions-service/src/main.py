47dfb1e0b2778d4083a90cfc042946aeb7b423a2
Kirill67tyar <kirillbogomolov.ric@gmail.com>
2025-06-11 16:43:23 +0300

    Дабавляю отдельный docker compose для сервиса content-actions.

--- services/content-actions-service/src/main.py
+++ services/content-actions-service/src/main.py
@@ -4,9 +4,6 @@ from api.v1.review import review_api
 from core.config import app_config
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-from rate_limite_utils import RequestContextMiddleware
-from tracer_utils import init_tracer, request_id_middleware
 from utils.connectors import lifespan
 from utils.exceptions_handlers import setup_exception_handlers
 
@@ -22,17 +19,6 @@ app = FastAPI(
 
 # Подключение обработчиков
 setup_exception_handlers(app)
-
-# Добавляю миддлвар для доступа Request во всех эндпоинтах
-app.add_middleware(RequestContextMiddleware)
-
-if app_config.tracing:
-    # Добавляем middleware
-    app.middleware("http")(request_id_middleware)
-    # Инициализация трейсера
-    init_tracer(app, app_config.project_name)
-    # Добавлене инструментария FastAPI для трейсов
-    FastAPIInstrumentor.instrument_app(app)
 
 SERVICE_PATH = "/content-actions/api/v1/"
 app.include_router(like_api.router, prefix=f"{SERVICE_PATH}likes", tags=["Лайки"])
