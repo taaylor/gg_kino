13807a6c6b1424aa6890b5267bdfb142ff2b3b31
Maksim Ushakov <jw999coding@gmail.com>
2025-06-16 00:46:46 +0300

    Устраняю замечания mypy

--- services/content-actions-service/src/services/rating_service.py
+++ services/content-actions-service/src/services/rating_service.py
@@ -51,33 +51,34 @@ class RatingService:
         """
         str_film_id = str(film_id)
         cache_key = CACHE_KEY_AVG_RATING + str_film_id
-        avg_rating = await self.cache.get(cache_key)
+        avg_rating_cache = await self.cache.get(cache_key)
         user_score = await self.get_user_score(
             user_id=user_id,
             film_id=film_id,
         )
-        if avg_rating:
+        if avg_rating_cache:
             logger.debug(f"Рейтинг фильма {str_film_id} получен из кеша по ключу: {cache_key}")
             return AvgRatingResponse(
-                **json.loads(avg_rating),
+                **json.loads(avg_rating_cache),
                 user_score=user_score,
             )
+
         avg_rating = await self.repository.calculate_average_rating(
             self.repository.collection.film_id == film_id
         )
 
         if not avg_rating:
             return None
-        result = avg_rating[0]
+
         await self.cache.background_set(
             key=cache_key,
-            value=result.model_dump_json(),
+            value=avg_rating[0].model_dump_json(),
             expire=app_config.cache_expire_in_seconds,
         )
 
         logger.debug(f"Рейтинг фильма {str_film_id} будет сохранён в кеш по ключу {cache_key}.")
         return AvgRatingResponse(
-            **result.model_dump(),
+            **avg_rating[0].model_dump(),
             user_score=user_score,
         )
 
@@ -140,12 +141,15 @@ class RatingService:
         avg_rating = await self.repository.calculate_average_rating(
             self.repository.collection.film_id == film_id
         )
-        await self.cache.background_set(
-            key=cache_key,
-            value=avg_rating[0].model_dump_json(),
-            expire=app_config.cache_expire_in_seconds,
-        )
-        logger.debug(f"Рейтинг фильма {str(film_id)} будет сохранён в кеш по ключу {cache_key}.")
+        if avg_rating:
+            await self.cache.background_set(
+                key=cache_key,
+                value=avg_rating[0].model_dump_json(),
+                expire=app_config.cache_expire_in_seconds,
+            )
+            logger.debug(
+                f"Рейтинг фильма {str(film_id)} будет сохранён в кеш по ключу {cache_key}."
+            )
         return result
 
     async def delete_user_score(self, user_id: UUID, film_id: UUID) -> None:
@@ -169,14 +173,15 @@ class RatingService:
             avg_rating = await self.repository.calculate_average_rating(
                 self.repository.collection.film_id == film_id
             )
-            await self.cache.background_set(
-                key=cache_key,
-                value=avg_rating[0].model_dump_json(),
-                expire=app_config.cache_expire_in_seconds,
-            )
-            logger.debug(
-                f"Рейтинг фильма {str(film_id)} будет сохранён в кеш по ключу {cache_key}."
-            )
+            if avg_rating:
+                await self.cache.background_set(
+                    key=cache_key,
+                    value=avg_rating[0].model_dump_json(),
+                    expire=app_config.cache_expire_in_seconds,
+                )
+                logger.debug(
+                    f"Рейтинг фильма {str(film_id)} будет сохранён в кеш по ключу {cache_key}."
+                )
 
 
 @lru_cache()
