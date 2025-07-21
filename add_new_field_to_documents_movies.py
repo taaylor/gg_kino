8d56113258c0aa66eac2fdb21283005758e3d74c
Kirill67tyar <kirillbogomolov.ric@gmail.com>
2025-07-21 23:30:20 +0300

    Добавляю фикстурам для индекса movies поле created_at и updated_at (файл movies_data.json)

---
+++ add_new_field_to_documents_movies.py
@@ -0,0 +1,24 @@
+import json
+import time
+
+PATH_TO_DOCUMENTS = "backup/elastic_dump/dump/movies_data.json"
+PATH_TO_NEW_DOCUMENTS_WITH_DATES = "backup/elastic_dump/dump/movies_data_with_dates.json"
+
+
+def main():
+    with (
+        open(file=PATH_TO_DOCUMENTS, mode="r", encoding="utf-8") as f_read,
+        open(file=PATH_TO_NEW_DOCUMENTS_WITH_DATES, mode="w", encoding="utf-8") as f_write,
+    ):
+        fims_in_str = f_read.readlines()
+        fims_in_dict = [json.loads(film) for film in fims_in_str]
+        # future_timestamp - на один день вперёд
+        future_timestamp = int(time.time() * 1000) + 100000000
+        for film in fims_in_dict:
+            film["_source"]["created_at"] = future_timestamp
+            film["_source"]["updated_at"] = future_timestamp
+            f_write.write(json.dumps(film, ensure_ascii=False) + "\n")
+
+
+if __name__ == "__main__":
+    main()
