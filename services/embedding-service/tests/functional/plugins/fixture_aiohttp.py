4d1101daf88705de8f2969b2e7eb7f0b9ae60d4c
Kirill67tyar <kirillbogomolov.ric@gmail.com>
2025-08-02 15:13:24 +0300

    Настраиваю стуктуру для тестов embedding-service

---
+++ services/embedding-service/tests/functional/plugins/fixture_aiohttp.py
@@ -0,0 +1,49 @@
+from json import JSONDecodeError
+
+import aiohttp
+import pytest_asyncio
+
+
+@pytest_asyncio.fixture(scope="session")
+async def aiohttp_session():
+    session = aiohttp.ClientSession()
+    yield session
+    await session.close()
+
+
+@pytest_asyncio.fixture(name="make_get_request")
+def make_get_request(aiohttp_session: aiohttp.ClientSession):
+    async def inner(
+        url: str,
+        params: dict | None = None,
+        headers: dict | None = None,
+    ) -> tuple[list | dict, int]:
+        # url = test_conf.asyncapi.host_service + uri
+        async with aiohttp_session.get(url, params=params, headers=headers) as response:
+            try:
+                body = await response.json()
+            except (JSONDecodeError, TypeError, aiohttp.ContentTypeError):
+                body = []
+            status = response.status
+        return body, status
+
+    return inner
+
+
+@pytest_asyncio.fixture(name="make_post_request")
+def make_post_request(aiohttp_session: aiohttp.ClientSession):
+    async def inner(
+        url: str,
+        data: dict | None = None,
+        params: dict | None = None,
+    ) -> tuple[list | dict, int]:
+        # url = test_conf.authapi.host_service + uri
+        async with aiohttp_session.post(url, json=data, params=params) as response:
+            try:
+                body = await response.json()
+            except (JSONDecodeError, TypeError, aiohttp.ContentTypeError):
+                body = []
+            status = response.status
+        return body, status
+
+    return inner
